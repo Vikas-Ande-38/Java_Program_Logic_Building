3a7dc8a41d7f94f4f93e82f882e0cd895ab38b65
Vikas-Ande-38 <2100030012cser@gmail.com>
2025-08-07 13:29:40 +0530

    Add files via upload

---
+++ CharType.java
@@ -0,0 +1,9 @@
+public class CharType {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char numberChar = '1';
+
+        System.out.println("Character: " + letter);
+        System.out.println("Another Character: " + numberChar);
+    }
+}
